5d4228b3be9f1cb5717283df669c454a05215023
stephen.kingsley <stephenkingsley@foxmail.com>
2016-08-29 10:50:50 +0800

    add cursor style in move

--- main.js
+++ main.js
@@ -37,8 +37,7 @@ function createMainWindow () {
   const maxWidthValue = 550;
   const minWidthValue = 400;
 
-  // Create the browser window.
-  const win = new BrowserWindow({
+	const defaultConfig = {
     title: app.getName(),
     show: false,
     x: lastWindowState.x,
@@ -59,7 +58,9 @@ function createMainWindow () {
       preload: path.join(__dirname, 'browser.js'),
       nodeIntegration: false
     }
-  });
+  };
+  // Create the browser window.
+  const win = new BrowserWindow(defaultConfig);
 
   win.webContents.setUserAgent(userAgent);
   win.loadURL(`https://www.instagram.com`);
