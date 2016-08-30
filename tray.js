108f9e04683b33488afdecb05870bfcb3ca76ed4
stephen.kingsley <stephenkingsley@foxmail.com>
2016-08-30 18:09:58 +0800

    package shell

--- tray.js
+++ tray.js
@@ -1,11 +1,10 @@
-'use strict';
-const path     = require('path');
+const path = require('path');
 const electron = require('electron');
 
-const app   = electron.app;
+const app = electron.app;
 const shell = electron.shell;
-const Tray  = electron.Tray;
-const Menu  = electron.Menu;
+const Tray = electron.Tray;
+const Menu = electron.Menu;
 
 const rammeTrayDefaultIcon = path.join(__dirname, 'static/icon-18x18.png');
 const rammeTrayWindowsIcon = path.join(__dirname, 'static/icon.ico');
@@ -28,37 +27,46 @@ exports.create = win => {
   const contextMenu = [{
     label: 'Toggle',
     click() {
-      !win.isMinimized() ? win.minimize() : win.show();
+      if (!win.isMinimized()) {
+        win.minimize();
+      } else {
+        win.show();
+      }
     }
   },
-  {
-    type: 'separator'
-  },
-  {
-    label: 'GitHub',
-    click() {
-      shell.openExternal('https://github.com/terkelg/ramme');
+    {
+      type: 'separator'
+    },
+    {
+      label: 'GitHub',
+      click() {
+        shell.openExternal('https://github.com/terkelg/ramme');
+      }
+    },
+    {
+      label: 'Issue',
+      click() {
+        shell.openExternal('https://github.com/terkelg/ramme/issues');
+      }
+    },
+    {
+      type: 'separator'
+    },
+    {
+      label: 'Quit',
+      click() {
+        app.quit();
+      }
     }
-  },
-  {
-    label: 'Issue',
-    click() {
-      shell.openExternal('https://github.com/terkelg/ramme/issues');
-    }
-  },
-  {
-    type: 'separator'
-  },
-  {
-    label: 'Quit',
-    click() {
-      app.quit();
-    }
-  }];
+  ];
 
   tray.setToolTip('ramme');
   tray.setContextMenu(Menu.buildFromTemplate(contextMenu));
   tray.on('click', () => {
-    win.isVisible() ? win.hide() : win.show();
+    if (win.isVisible()) {
+      win.hide();
+    } else {
+      win.show();
+    }
   });
 };
