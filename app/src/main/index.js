a3c8e97e82efe5b62dbe50e0a79578d58606318b
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2017-05-24 22:56:08 +0200

    fixes all dark theme issues & change default window minWidth

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -86,7 +86,7 @@ function createMainWindow () {
   const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
   const rammeDesktopIcon = path.join(__dirname, '../static/icon.png')
   const maxWidthValue = 550
-  const minWidthValue = 400
+  const minWidthValue = 460
 
   // Create the browser window.
   const win = new BrowserWindow({
