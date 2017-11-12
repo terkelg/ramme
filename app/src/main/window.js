ffb3feefc44d69e95338e994fd60d758f5bb96a0
duxy1996 <durancdr96@gmail.com>
2017-11-12 13:53:22 +0100

    added back button in the main window

--- app/src/main/window.js
+++ app/src/main/window.js
@@ -9,6 +9,7 @@ const isPlatform = require('./../common/is-platform')
 
 let registeredWindows = {}
 let currentWindows = {}
+let win
 
 // [function] Register Window
 function registerWindow (name, def) {
@@ -42,6 +43,49 @@ function registerWindow (name, def) {
   return true
 }
 
+
+
+function intervalFunc() {
+  if (win) {
+        jscode = "var comp = document.getElementById('back_button');         \
+                  if (comp == null){                                         \
+                  var element = document.getElementsByClassName('_tdn3u');   \
+                  var para = document.createElement('div');                  \
+                  para.setAttribute('class', '_k0d2z' );                     \
+                  para.setAttribute('id', 'back_button' );                   \
+                  var node = document.createTextNode('');                    \
+                  para.appendChild(node);                                    \
+                  element[0].appendChild(para);                              \
+                  var element = document.getElementById('back_button');      \
+                  var para = document.createElement('a');                    \
+                  para.setAttribute('class', '_ttgfw' );                     \
+                  para.setAttribute('id', 'back_button_a' );                 \
+                  para.setAttribute('onclick','goBack()');                   \
+                  element.appendChild(para);                                 \
+                  var element = document.getElementById('back_button_a');    \
+                  var para = document.createElement('div');                  \
+                  para.setAttribute('class', '_crp8c' );                     \
+                  para.setAttribute('id', 'back_button_div' );               \
+                  para.setAttribute('style', 'background-color: #262626;     \
+                                              border-radius: 50%;            \
+                                              height: 24px; width: 24px;' ); \
+                  element.appendChild(para);                                 \
+                  var para = document.createElement('script');               \
+                  para.setAttribute('type', 'text/javascript' );             \
+                  var node = document.createTextNode('function goBack() {    \
+                                     window.history.back();                  \
+                                                                         }');\
+                  para.appendChild(node);                                    \
+                  document.head.appendChild(para);}                          \
+                                                                             \
+        "
+        win.webContents.executeJavaScript(jscode)
+
+    }
+}
+
+setInterval(intervalFunc, 50);
+
 // [function] Open Window
 function openWindow (name) {
   if (registeredWindows[name]) {
@@ -61,15 +105,16 @@ function openWindow (name) {
     // Hide window until it is ready
     wanted.def.show = false
 
-    const win = new BrowserWindow(wanted.def)
+    win = new BrowserWindow(wanted.def)
+       
 
     if (win) {
-      if (wanted.options.agent) {
-        win.webContents.setUserAgent(wanted.options.agent)
+      if (wanted.options.agent) {        
+        win.webContents.setUserAgent(wanted.options.agent)   
       }
 
       // Load URL
-      if (wanted.options.url) {
+      if (wanted.options.url) {        
         win.loadURL(wanted.options.url)
       }
 
@@ -157,6 +202,7 @@ ipcMain.on('window-close', function (event, name) {
 })
 
 ipcMain.on('window-load', function (event, name, url) {
+  console.log("Hope")
   loadURL(name, url)
 })
 
