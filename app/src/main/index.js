0846403dc54cd6ede71bc6befce8387b5df0cf44
Anatoli Nicolae <nicolaeanatoli@gmail.com>
2018-07-02 11:47:54 +0200

    Not the best, not the worst update.
    Signed-off-by: Anatoli Nicolae <nicolaeanatoli@gmail.com>

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -40,13 +40,13 @@ if (shouldQuit) {
  */
 
 window.register('main', {
-  url: 'https://www.instagram.com/',
+  url: 'https://www.instagram.com/?utm_source=ig_lite',
   useLastState: true,
   fakeUserAgent: true,
   defaultWindowEvents: false,
   show: false,
-  minHeight: 400,
-  minWidth: 460,
+  minHeight: 480,
+  minWidth: 380,
   maxWidth: 550,
   maximizable: false,
   fullscreenable: false,
@@ -112,7 +112,9 @@ ipcMain.on('back', (e, arg) => {
 
 ipcMain.on('home', (e, arg) => {
   let page = e.sender.webContents
-  page.loadURL('https://www.instagram.com/')
+  page.loadURL('https://www.instagram.com/?utm_source=ig_lite', {
+    userAgent: 'Mozilla/5.0 (Linux; Android 8.0.0; Android SDK built for x86 Build/OSR1.170901.043; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.158 Mobile Safari/537.36 InstagramLite 1.0.0.0.145 Android (26/8.0.0; 420dpi; 1080x1794; Google/google; Android SDK built for x86; generic_x86; ranchu; en_US; 115357035)'
+  })
   page.clearHistory()
 })
 
